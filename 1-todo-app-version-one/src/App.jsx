f016fd925c4c6e21df1303929a3acf0f0fbb0166
Sakshi Raj <sakshiraj1705@gmail.com>
2024-08-30 02:12:46 +0530

    todo-project-version-one

---
+++ 1-todo-app-version-one/src/App.jsx
@@ -0,0 +1,18 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+import "./App.css";
+
+function App() {
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo />
+      <TodoItem1 />
+      <TodoItem2 />
+    </center>
+  );
+}
+
+export default App;
---
+++ 1-todo-app-version-one/src/components/TodoItem1.jsx
@@ -0,0 +1,20 @@
+function TodoItem1() {
+  let todoName = "Buy Milk";
+  let tododate = "4/10/23";
+  return (
+    <div className="container">
+      <div className="row kg-row">
+        <div className="col-6">{todoName}</div>
+        <div className="col-4">{tododate}</div>
+
+        <div className="col-2">
+          <button type="button" className="btn btn-danger kg-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem1;
