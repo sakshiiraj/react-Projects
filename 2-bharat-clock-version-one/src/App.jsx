195668f4361ae2b640a434ec9581abdb2867850c
Sakshi Raj <sakshiraj1705@gmail.com>
2024-08-30 02:39:06 +0530

    2-bharat-clock-version-one

---
+++ 2-bharat-clock-version-one/src/App.jsx
@@ -0,0 +1,16 @@
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import CurrentTime from "./components/CurrentTime";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <center>
+      <ClockHeading />
+      <ClockSlogan />
+      <CurrentTime />
+    </center>
+  );
+}
+
+export default App;
